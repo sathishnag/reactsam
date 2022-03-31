e089ca5940025f18b742dfd929f90d71afa59f43
unknown <sathish.auttpc@gmail.com>
2022-03-31 18:46:20 +0530

    'changescommited'

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import EditableTable from "./EditableTable";
+import fieldsArr from "./fields";
+import "./styles.css";
 
-import App from "./App";
+const defaultData = [
+  {
+    name: "Yash",
+    age: "23",
+    relationShip: "haqSeSingle"
+  }
+];
+function App() {
+  const getData = row => {
+    console.log(row, "rows data");
+  };
+
+  return (
+    <div className="App">
+      <h1>Editable Table</h1>
+      <EditableTable
+        initWithoutHead
+        defaultData={defaultData}
+        getData={getData}
+        fieldsArr={fieldsArr}
+      />
+    </div>
+  );
+}
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  rootElement
-);
+ReactDOM.render(<App />, rootElement);
